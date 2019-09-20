702aa651874f1423a38a9ef75047e86eeb05c65e
nillithian <macrid@gmail.com>
2019-09-20 18:26:08 +0300

    commit 2

--- src/com/company/SumOddRange.java
+++ src/com/company/SumOddRange.java
@@ -1,4 +1,33 @@
 package com.company;
 
 public class SumOddRange {
+
+
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        } else return number % 2 != 0;
+
+    }
+
+
+    public static int sumOdd(int start, int end) {
+        int sum=0;
+        for (int i = start ;i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+                if (start == end){
+                    System.out.println("the sum is = " + sum);
+                    break;
+                }
+//                System.out.println("the sum is = " + sum);
+
+
+
+            }
+
+
+        }
+        return -1;
+    }
 }
